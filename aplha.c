3853d84c35a855fd2995cf88fb1568978d092651
Manikandaprabh <manikandaprabhu.scse@ksrce.ac.in>
2017-06-05 12:16:20 +0530

    Add files via upload

---
+++ aplha.c
@@ -0,0 +1,12 @@
+#include,stdio.h>
+void main()
+{
+ char()
+printf("Enter a character");
+scanf("%c",&c);
+if(c>='a'&&c>='z'))
+printf("%c is alphabet",c);
+else
+printf("%c is not alphabet",c);
+return 0;
+}
